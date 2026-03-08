476f952b4dda60a11730410654ab22b6f6e388fa
Joan Matias <yoan16908@gmail.com>
2026-03-08 16:02:40 -0400

    Add community plugins for AstroNvim

---
+++ nvim/lua/plugins/community.lua
@@ -0,0 +1,6 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.tailwindcss" },
+}
